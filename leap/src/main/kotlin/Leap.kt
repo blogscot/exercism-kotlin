6fd210c6676bfc1cbd74f52625c42fd47c0303bb
Iain Diamond <iain.diamond@gmail.com>
2018-11-23 19:08:43 +0000

    Add a lambda for fun

--- leap/src/main/kotlin/Leap.kt
+++ leap/src/main/kotlin/Leap.kt
@@ -1,6 +1,6 @@
 class Year(private val year: Int) {
 
-  val isLeap
-    get() =
-      year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+  private val isDivisibleBy: (Int) -> Boolean = { year % it == 0 }
+
+  val isLeap = isDivisibleBy(4) and !isDivisibleBy(100) or isDivisibleBy(400)
 }
