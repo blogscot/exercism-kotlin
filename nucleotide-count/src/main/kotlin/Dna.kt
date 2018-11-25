adc6d9206da8a19aef9a7810e70c2e32ec943e1c
Iain Diamond <iain.diamond@gmail.com>
2018-11-25 17:04:18 +0000

    Prefer groupBy

--- nucleotide-count/src/main/kotlin/Dna.kt
+++ nucleotide-count/src/main/kotlin/Dna.kt
@@ -2,14 +2,12 @@ class Dna(strand: String) {
   var nucleotideCounts = mutableMapOf('A' to 0, 'C' to 0, 'G' to 0, 'T' to 0)
 
   init {
-    nucleotideCounts = strand.fold(nucleotideCounts) { acc, nucleotide ->
+    require(strand.all { it in "AGCT" }) { "Invalid argument: $strand" }
 
-      when (nucleotide) {
-        !in "AGCT" ->
-          throw IllegalArgumentException("Invalid nucleotide: $nucleotide")
-        in nucleotideCounts -> acc[nucleotide] = acc[nucleotide]!! + 1
-        else -> acc[nucleotide] = 1
-      }
+    val nucleotides = strand.groupBy { it }.mapValues { it.value.size }
+
+    nucleotideCounts = nucleotides.entries.fold(nucleotideCounts) { acc, (key, value) ->
+      acc[key] = value
       acc
     }
   }
--- nucleotide-count/src/test/kotlin/DnaTest.kt
+++ nucleotide-count/src/test/kotlin/DnaTest.kt
@@ -1,4 +1,5 @@
 import org.junit.Test
+import org.junit.Ignore
 import kotlin.test.assertEquals
 
 class DnaTest {
