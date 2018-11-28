1f9fa6149b2b26454ce9d39ce9d01b63962e2438
Iain Diamond <iain.diamond@gmail.com>
2018-11-28 11:53:58 +0000

    Refactor and provide optional decimal place setting

--- space-age/src/main/kotlin/SpaceAge.kt
+++ space-age/src/main/kotlin/SpaceAge.kt
@@ -2,21 +2,22 @@ import java.math.BigDecimal
 
 private const val EARTH_PERIOD = 31557600.0
 
-class SpaceAge(seconds: Long) {
+class SpaceAge(private val seconds: Long, private val decimalPlaces: Int = 2) {
 
-  private val earthRotations = seconds / EARTH_PERIOD
+  private fun calculateAge(ratio: Double): Double =
+      round(seconds / EARTH_PERIOD / ratio)
 
-  fun onEarth() = earthRotations.round()
-  fun onMercury() = (earthRotations / 0.2408467).round()
-  fun onVenus() = (earthRotations / 0.61519726).round()
-  fun onMars() = (earthRotations / 1.8808158).round()
-  fun onJupiter() = (earthRotations / 11.862615).round()
-  fun onSaturn() = (earthRotations / 29.447498).round()
-  fun onUranus() = (earthRotations / 84.016846).round()
-  fun onNeptune() = (earthRotations / 164.79132).round()
+  fun onEarth() = calculateAge(1.0)
+  fun onMercury() = calculateAge(0.2408467)
+  fun onVenus() = calculateAge(0.61519726)
+  fun onMars() = calculateAge(1.8808158)
+  fun onJupiter() = calculateAge(11.862615)
+  fun onSaturn() = calculateAge(29.447498)
+  fun onUranus() = calculateAge(84.016846)
+  fun onNeptune() = calculateAge(164.79132)
 
-  private fun Double.round(places: Int = 2) =
-    BigDecimal(toString())
+  private fun round(value: Double, places: Int = decimalPlaces) =
+    BigDecimal(value.toString())
         .setScale(places, BigDecimal.ROUND_HALF_UP)
         .toDouble()
 }
