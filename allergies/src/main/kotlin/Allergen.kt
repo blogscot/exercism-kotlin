ff7a48246031b4b3b13a81494b133ef4e5fa2d93
Iain Diamond <iain.diamond@gmail.com>
2018-11-28 12:49:13 +0000

    Add Allergies solution

---
+++ allergies/src/main/kotlin/Allergen.kt
@@ -0,0 +1,23 @@
+enum class Allergen(val score: Int) {
+  EGGS(1),
+  PEANUTS(2),
+  SHELLFISH(4),
+  STRAWBERRIES(8),
+  TOMATOES(16),
+  CHOCOLATE(32),
+  POLLEN(64),
+  CATS(128)
+}
+
+class Allergies(private val score: Int) {
+
+  fun isAllergicTo(allergen: Allergen) =
+      allergen.score and score == allergen.score
+
+
+  fun getList() =
+      Allergen.values()
+          .asList()
+          .filter { (it.score and score) == it.score }
+
+}
