9bb92990507a8ed3e366562087fd370cf207df25
Iain Diamond <iain.diamond@gmail.com>
2018-12-08 14:34:38 +0000

    Refactor using apply

--- secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,15 +1,11 @@
 object HandshakeCalculator {
 
-  fun calculateHandshake(num: Int): List<Signal> {
-    val signals = mutableListOf<Signal>()
-
-    if (num and 1 > 0) signals.add(Signal.WINK)
-    if (num and 2 > 0) signals.add(Signal.DOUBLE_BLINK)
-    if (num and 4 > 0) signals.add(Signal.CLOSE_YOUR_EYES)
-    if (num and 8 > 0) signals.add(Signal.JUMP)
-    if (num and 16 > 0) signals.reverse()
-
-    return signals
+  fun calculateHandshake(num: Int): List<Signal> =
+    mutableListOf<Signal>().apply {
+      if (num and 1 > 0) add(Signal.WINK)
+      if (num and 2 > 0) add(Signal.DOUBLE_BLINK)
+      if (num and 4 > 0) add(Signal.CLOSE_YOUR_EYES)
+      if (num and 8 > 0) add(Signal.JUMP)
+      if (num and 16 > 0) reverse()
   }
 }
-
