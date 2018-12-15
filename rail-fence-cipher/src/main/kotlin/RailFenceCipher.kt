73a80ff49a9420d0061471ce31fdb2fafc3046fd
Iain Diamond <iain.diamond@gmail.com>
2018-12-15 13:18:49 +0000

    fix formatting

--- rail-fence-cipher/src/main/kotlin/RailFenceCipher.kt
+++ rail-fence-cipher/src/main/kotlin/RailFenceCipher.kt
@@ -3,9 +3,9 @@ class RailFenceCipher(private val num: Int) {
 
   fun getEncryptedData(message: String) =
 
-    getEncoding(message)
-        .fold(StringBuffer()) { acc, index -> acc.append(message[index]) }
-        .toString()
+      getEncoding(message)
+          .fold(StringBuffer()) { acc, index -> acc.append(message[index]) }
+          .toString()
 
   fun getDecryptedData(text: String): String {
 
@@ -15,16 +15,16 @@ class RailFenceCipher(private val num: Int) {
         .map { it.first }
 
     return decoding
-        .fold(StringBuffer()) { acc, index -> acc.append(text[index])}
+        .fold(StringBuffer()) { acc, index -> acc.append(text[index]) }
         .toString()
   }
 
   private fun getEncoding(message: String): List<Int> =
-    cycle(num - 1)
-        .zip(numbers)
-        .take(message.length)
-        .groupBy({ it.first }, { it.second })
-        .values.flatten()
+      cycle(num - 1)
+          .zip(numbers)
+          .take(message.length)
+          .groupBy({ it.first }, { it.second })
+          .values.flatten()
 
 }
 
