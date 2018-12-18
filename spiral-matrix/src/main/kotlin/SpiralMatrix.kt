62fd1f0335cdc5a001fbc81086438b7f5aa9b900
Iain Diamond <iain.diamond@gmail.com>
2018-12-18 13:03:12 +0000

    Make code more DRY

--- spiral-matrix/src/main/kotlin/SpiralMatrix.kt
+++ spiral-matrix/src/main/kotlin/SpiralMatrix.kt
@@ -2,48 +2,34 @@ object SpiralMatrix {
 
   fun ofSize(size: Int): Array<IntArray> {
 
-    val arr = Array(size) { IntArray(size) { 0 } }
+    val matrix = Array(size) { IntArray(size) { 0 } }
 
     var row = 0
     var col = -1
     var num = 1
-    var count: Int
     var moves = size
 
-    while (moves > 0 && num <= (size * size)) {
+    val right = { col++ }
+    val left = { col-- }
+    val up = { row-- }
+    val down = { row++ }
 
-      // move right
-      count = moves
-      while (count-- > 0) {
-        col++
-        arr[row][col] = num++
-      }
-
-      // move down
-      moves -= 1
-      count = moves
+    val move = { action: () -> Int, totalMoves: Int ->
+      var count = totalMoves
 
       while (count-- > 0) {
-        row++
-        arr[row][col] = num++
-      }
-
-      // move left
-      count = moves
-      while (count-- > 0) {
-        col--
-        arr[row][col] = num++
-      }
-
-      // move up
-      moves -= 1
-      count = moves
-      while (count-- > 0) {
-        row--
-        arr[row][col] = num++
+        action()
+        matrix[row][col] = num++
       }
     }
 
-    return arr
+    while (num <= (size * size)) {
+      move(right, moves)
+      move(down, --moves)
+      move(left, moves)
+      move(up, --moves)
+    }
+
+    return matrix
   }
 }
