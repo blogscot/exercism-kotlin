8e60a11d63517f69825d4ccef37903f5f48bb4c5
Iain Diamond <iain.diamond@gmail.com>
2018-12-19 11:35:35 +0000

    Refactor to use a sequence

--- pascals-triangle/src/main/kotlin/PascalsTriangle.kt
+++ pascals-triangle/src/main/kotlin/PascalsTriangle.kt
@@ -3,15 +3,7 @@ object PascalsTriangle {
   fun computeTriangle(num: Int): List<List<Int>> {
     require(num >= 0) { "Rows can't be negative!" }
 
-    if (num == 0) return emptyList()
-
-    var line = listOf(1)
-    val output = mutableListOf(line)
-
-    repeat(num - 1) {
-      line = listOf(1) + line.zipWithNext().map { (a, b) -> a + b } + listOf(1)
-      output.add(line)
-    }
-    return output
+    return generateSequence(listOf(1)) { listOf(1) + it.zipWithNext().map { (a, b) -> a + b } + listOf(1) }
+        .take(num).toList()
   }
 }
