87a8fce387a6dd87f9252b99aff5c8e4ca6da945
Iain Diamond <iain.diamond@gmail.com>
2018-12-20 12:37:10 +0000

    Minor refactoring

--- rotational-cipher/src/main/kotlin/RotationalCipher.kt
+++ rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -1,20 +1,16 @@
 class RotationalCipher(key: Int) {
 
-  private val alphabetLowercase = 'a'..'z'
-  private val alphabetUppercase = 'A'..'Z'
-  private val rotLower = rotate(key, alphabetLowercase)
-  private val rotUpper = rotate(key, alphabetUppercase)
+  private val aToZLower = ('a'..'z').toList()
+  private val aToZUpper = ('A'..'Z').toList()
+  private val rotLower = (aToZLower.drop(key) + aToZLower.take(key)).joinToString("")
+  private val rotUpper = rotLower.map(Char::toUpperCase)
 
   fun encode(text: String) =
-
       text.map { letter ->
         when (letter) {
-          in alphabetLowercase ->rotLower[alphabetLowercase.toList().indexOf(letter)]
-          in alphabetUppercase ->rotUpper[alphabetUppercase.toList().indexOf(letter)]
+          in aToZLower -> rotLower[aToZLower.indexOf(letter)]
+          in aToZUpper -> rotUpper[aToZUpper.indexOf(letter)]
           else -> letter
         }
       }.joinToString("")
-
-  private fun rotate(num: Int, text: CharRange) =
-      (text.drop(num) + text.take(num)).joinToString("")
 }
