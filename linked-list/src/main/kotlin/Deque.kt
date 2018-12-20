a43f79e647f21ab2784bef56912d6fdd16b50067
Iain Diamond <iain.diamond@gmail.com>
2018-12-20 14:01:28 +0000

    Implement using Nodes

--- linked-list/src/main/kotlin/Deque.kt
+++ linked-list/src/main/kotlin/Deque.kt
@@ -1,8 +1,45 @@
-class Deque<T> {
-  private val deque = mutableListOf<T>()
+data class Node<T>(val data: T, var next: Node<T>? = null, var previous: Node<T>? = null)
 
-  fun push(element: T) = deque.add(element)
-  fun pop(): T = deque.removeAt(deque.size - 1)
-  fun shift(): T = deque.removeAt(0)
-  fun unshift(element: T) = deque.add(0, element)
+class Deque<T> {
+
+  var head: Node<T>? = null
+  var tail: Node<T>? = null
+
+  fun push(entry: T) {
+    if (head == null) {
+      val newNode = Node(entry)
+      head = newNode
+      tail = newNode
+    } else {
+      // push entry onto end
+      val newNode = Node(entry, previous = tail)
+      tail?.next = newNode
+      tail = newNode
+    }
+  }
+
+  fun pop(): T? {
+    val data = tail?.data
+    tail = tail?.previous
+    return data
+  }
+
+  fun shift(): T? {
+    val data = head?.data
+    head = head?.next
+    return data
+  }
+
+  fun unshift(entry: T) {
+    if (head == null) {
+      val newNode = Node(entry)
+      head = newNode
+      tail = newNode
+    } else {
+      // push entry onto start
+      val newNode = Node(entry, next = head)
+      head?.previous = newNode
+      head = newNode
+    }
+  }
 }
