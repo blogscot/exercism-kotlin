0eb9f28c1c7034cd8c690568b984f659e82c3d96
Iain Diamond <iain.diamond@gmail.com>
2018-12-22 14:33:25 +0000

    Split out character transform into new function

--- minesweeper/src/main/kotlin/MinesweeperBoard.kt
+++ minesweeper/src/main/kotlin/MinesweeperBoard.kt
@@ -1,6 +1,3 @@
-data class Position(val x: Int, val y: Int)
-
-
 class MinesweeperBoard(private val board: List<String>) {
   private val neighbours = listOf(
       -1 to -1, 0 to -1, 1 to -1,
@@ -9,18 +6,21 @@ class MinesweeperBoard(private val board: List<String>) {
   )
 
   fun withNumbers(): List<String> =
-      board.mapIndexed { rowIndex, x ->
-        x.withIndex().joinToString("") { (colIndex, square) ->
-          if (square != '*') {
-            val neighbours = calculateNeighbours(Position(rowIndex, colIndex))
-            if (neighbours > 0) neighbours.toString() else " "
-          } else "*"
+      board.mapIndexed { rowIndex, row ->
+        row.withIndex().joinToString("") { (colIndex, square) ->
+          findNewSquare(square, Pair(rowIndex, colIndex))
         }
       }
 
-  private fun calculateNeighbours(pos: Position): Int =
+  private fun findNewSquare(square: Char, pos: Pair<Int, Int>) =
+    if (square != '*') {
+      val numNeighbours = calculateNeighbours(pos)
+      if (numNeighbours > 0) numNeighbours.toString() else " "
+    } else "*"
+
+  private fun calculateNeighbours(pos: Pair<Int, Int>): Int =
       neighbours
-          .map { (x, y) -> Pair(pos.x + x, pos.y + y) }
+          .map { (x, y) -> Pair(pos.first + x, pos.second + y) }
           .filter { (x, y) -> x in 0 until board.size && y in 0 until board[0].length }
           .fold(0) { acc, (x, y) ->
             if (board[x][y] == '*') acc + 1 else acc
