01c2679ee7a2a287ade831f536844a8ee1bb3739
Iain Diamond <iain.diamond@gmail.com>
2019-01-02 14:16:15 +0000

    replace substring

--- pig-latin/src/main/kotlin/PigLatin.kt
+++ pig-latin/src/main/kotlin/PigLatin.kt
@@ -8,6 +8,6 @@ private fun translateWord(word: String) =
       word.matches("(yt|xr|[aeiou]|[aeiou]qu).*".toRegex()) -> word + "ay"
       word.matches("(thr|sch|[^aeiou]qu).*".toRegex()) -> word.drop(3) + word.take(3) + "ay"
       word.matches("(ch|qu|th|rh).*".toRegex()) -> word.drop(2) + word.take(2) + "ay"
-      else -> word.substring(1) + word.first() + "ay"
+      else -> word.drop(1) + word.first() + "ay"
     }
 
