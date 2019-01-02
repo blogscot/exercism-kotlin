a1ccab4c2f05922c3da8776c9ff0e89abf9775b3
Iain Diamond <iain.diamond@gmail.com>
2019-01-02 14:11:30 +0000

    Reimplement using regex matches

--- pig-latin/src/main/kotlin/PigLatin.kt
+++ pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,28 +1,13 @@
 package PigLatin
 
-private fun translateWord(word: String): String {
-  val vowels = "aeiouy"
-  val firstLetter = word[0]
-  val secondLetter = word[1]
-  val firstTwoLetters = word.substring(0,2)
-
-  if (firstLetter == 'y' && secondLetter in vowels) {
-    return word.substring(1) + "yay"
-  }
-
-  if (firstLetter in vowels || firstTwoLetters == "xr" || firstTwoLetters == "yt") {
-    return word + "ay"
-  } 
-
-  val vowelPosition = word.indexOfFirst { it in vowels }
-  val (prefix, suffix) = if (vowelPosition > 0 && word.substring(vowelPosition - 1, vowelPosition + 1) == "qu") {
-    Pair(word.substring(0, vowelPosition + 1),  word.substring(vowelPosition + 1))
-  } else {
-    Pair(word.takeWhile { it !in vowels }, word.dropWhile { it !in vowels })
-  }
-
-  return suffix + prefix + "ay"
-}
-
 fun translate(text: String): String =
-  text.split(" ").map { translateWord(it) }.joinToString(" ")
+    text.split(" ").joinToString(" ") { translateWord(it) }
+
+private fun translateWord(word: String) =
+    when {
+      word.matches("(yt|xr|[aeiou]|[aeiou]qu).*".toRegex()) -> word + "ay"
+      word.matches("(thr|sch|[^aeiou]qu).*".toRegex()) -> word.drop(3) + word.take(3) + "ay"
+      word.matches("(ch|qu|th|rh).*".toRegex()) -> word.drop(2) + word.take(2) + "ay"
+      else -> word.substring(1) + word.first() + "ay"
+    }
+
